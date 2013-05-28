0105ad6bcda1a81bc87e75563b218c25a5f72ad4
hakre <hanskrentel@yahoo.de>
2013-05-28 10:07:50 +0200

    Added FunctionIterator

---
+++ src/FunctionIterator.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Iterator Garden
+ */
+
+/**
+ * Class ApplyIterator
+ *
+ * Iteration of a function until it returns a false-y value
+ */
+class FunctionIterator implements Iterator
+{
+    private $position;
+    private $current;
+    private $function;
+
+    public function __construct(callable $function)
+    {
+        $this->function = $function;
+    }
+
+    public function rewind()
+    {
+        if (NULL !== $this->position) {
+            return;
+        }
+        $this->position = 0;
+        $this->current  = call_user_func($this->function);
+    }
+
+    public function valid()
+    {
+        return (bool)$this->current;
+    }
+
+    public function current()
+    {
+        return $this->current;
+    }
+
+    public function key()
+    {
+        return $this->position;
+    }
+
+    public function next()
+    {
+        if (!$this->current) {
+            return;
+        }
+        $this->position++;
+        $this->current = call_user_func($this->function);
+    }
+}
---
+++ test/FunctionIteratorTest.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Iterator Garden
+ */
+
+class FunctionIteratorTest extends IteratorTestCase
+{
+    public function testConstructor()
+    {
+        $function = function () {
+            return FALSE;
+        };
+        $iter     = new FunctionIterator($function);
+        $this->assertInstanceOf('FunctionIterator', $iter);
+    }
+
+    public function testIteration()
+    {
+        $array = ['danger!', 'hello', 'world'];
+        $init  = function ($array) {
+            return function () use (&$array) {
+                return array_shift($array);
+            };
+        };
+        $iter  = new FunctionIterator($init($array));
+        $this->assertIterationValues($array, $iter);
+
+        // test norewind on invalidated iteration
+        $expected = $iter->key();
+        $iter->rewind();
+        $this->assertFalse($iter->valid());
+        $iter->next();
+        $this->assertSame($expected, $iter->key());
+    }
+}
