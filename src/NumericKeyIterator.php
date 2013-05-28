a4bf3ddc46c153dec0af5977872bc636359910a0
hakre <hanskrentel@yahoo.de>
2013-05-28 11:09:45 +0200

    Added NumericKeyIterator

---
+++ src/NumericKeyIterator.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Iterator Garden
+ */
+
+/**
+ * Class NumericKeyIterator
+ *
+ * Iteration that has an Index as key
+ */
+class NumericKeyIterator extends IndexIteration
+{
+    public function key()
+    {
+        return $this->getIndex();
+    }
+}
---
+++ test/NumericKeyIteratorTest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Iterator Garden
+ */
+
+class NumericKeyIteratorTest extends IteratorTestCase
+{
+    public function testIteration() {
+        $aToZ     = range('a', 'z');
+        $expected = new ArrayIterator($aToZ);
+
+        $subject  = new ArrayIterator(array_combine($aToZ, $aToZ));
+        $actual   = new NumericKeyIterator($subject);
+
+        $this->assertIteration($expected, $actual);
+    }
+}
