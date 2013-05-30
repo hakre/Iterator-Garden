1c9afcf9490bf68975c69dacac2e3ee9ef499d76
hakre <hanskrentel@yahoo.de>
2013-05-30 03:00:30 +0200

    Added PropertyIterator

---
+++ src/PropertyIterator.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * IteratorGarden
+ */
+
+/**
+ * Class PropertyIterator
+ *
+ * User-defined properties to iterate over an object
+ */
+class PropertyIterator extends TraversableDecorator
+{
+    private $object;
+    private $properties;
+
+    public function __construct($object, array $properties)
+    {
+        $this->object     = $object;
+        $this->properties = $properties;
+        parent::__construct(new ArrayIterator($properties));
+    }
+
+    public function current()
+    {
+        $name = parent::current();
+        return $this->object->$name;
+    }
+
+    public function key()
+    {
+        return parent::current();
+    }
+}
