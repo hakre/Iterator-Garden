de8b78e7142ba917a1cb02dddc4892d3d5ac4dae
hakre <hanskrentel@yahoo.de>
2013-05-30 03:00:10 +0200

    Added AggregatingIterator

---
+++ src/AggregatingIterator.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Iterator Garden
+ */
+
+/**
+ * Class AggregatingIterator
+ *
+ * Iterator that aggregates an Iterator on rewind()
+ */
+class AggregatingIterator extends TraversableDecorator
+{
+    private $iterator;
+
+    public function __construct(Traversable $iterator)
+    {
+        $this->iterator = $iterator;
+    }
+
+    public function getIterator()
+    {
+        return new IteratorIterator($this->iterator);
+    }
+
+    public function rewind()
+    {
+        $this->traversable = $this->getIterator();
+    }
+}
