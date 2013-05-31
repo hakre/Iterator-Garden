77ab485f6543142384a82f414aa844bdacc1d505
hakre <hanskrentel@yahoo.de>
2013-05-31 11:11:31 +0200

    Updated SeekableDebugIterator
    - Make use of TraversableDecorator protected member.

--- src/Debug/SeekableDebugIterator.php
+++ src/Debug/SeekableDebugIterator.php
@@ -28,27 +28,26 @@ class SeekableDebugIterator extends DebugIterator implements SeekableIterator
 
     public function __construct(SeekableIterator $it)
     {
-        $this->innerSeekableIterator = $it;
         parent::__construct($it);
     }
 
     public function current() {
         $this->event(__FUNCTION__ . '()');
-        $current = $this->innerSeekableIterator->current();
-        $this->event(sprintf('self::$innerSeekableIterator->current() is %s', $this->varLabel($current)));
+        $current = $this->traversable->current();
+        $this->event(sprintf('self::$innerSeekableIterator->current() is %s', self::debugVarLabel($current)));
         return $current;
     }
 
     public function key() {
         $this->event(__FUNCTION__ . '()');
-        $key = $this->innerSeekableIterator->key();
-        $this->event(sprintf('self::$innerSeekableIterator->key() is %s', $this->varLabel($key)));
+        $key = $this->traversable->key();
+        $this->event(sprintf('self::$innerSeekableIterator->key() is %s', self::debugVarLabel($key)));
         return $key;
 
     }
 
     public function seek($position) {
         $this->event(sprintf('%s(%d)', __FUNCTION__, $position));
-        $this->innerSeekableIterator->seek($position);
+        $this->traversable->seek($position);
     }
 }
