f3dbeab97449035c55c40bc22987a5dbece45565
hakre <hanskrentel@yahoo.de>
2013-06-30 12:29:54 +0200

    Added FetchingIterator

---
+++ src/FetchingIterator.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Iterator Garden
+ */
+
+/**
+ * Class FetchIterator
+ */
+class FetchingIterator implements Iterator
+{
+    private $callback;
+    private $index;
+    private $current;
+    private $valid;
+
+    public function __construct($callback)
+    {
+        if (!is_callable($callback, TRUE)) {
+            throw new InvalidArgumentException('Invalid callback given.');
+        }
+        $this->callback = $callback;
+    }
+
+    public function rewind()
+    {
+        $this->index = -1;
+        $this->fetchNext();
+    }
+
+    public function valid()
+    {
+        return $this->valid;
+    }
+
+    public function current()
+    {
+        return $this->current;
+    }
+
+    public function key()
+    {
+        return $this->index;
+    }
+
+    public function next()
+    {
+        $this->fetchNext();
+    }
+
+    protected function fetchNext()
+    {
+        $this->index++;
+        $this->valid = NULL !== $this->current = call_user_func($this->callback);
+    }
+}
---
+++ test/FetchingIteratorTest.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Iterator Garden
+ */
+
+class FetchingIteratorTest extends IteratorTestCase
+{
+
+    public function testConstructor()
+    {
+        $callback = $this->createCallbackWithValues();
+        $subject  = new FetchingIterator($callback);
+        $this->assertInstanceOf('FetchingIterator', $subject);
+    }
+
+    private function createCallbackWithValues(array $values = array())
+    {
+        $creator = function () use ($values) {
+            $stack = array_reverse($values);
+            return function () use (&$stack) {
+                return array_pop($stack);
+            };
+        };
+
+        return $creator();
+    }
+
+    public function testIteration()
+    {
+        $values = array('a', 'b', 'c');
+
+
+        $this->assertIterationValues(
+            $values,
+            new FetchingIterator($this->createCallbackWithValues($values))
+        );
+
+        $this->assertIteration(
+            new ArrayIterator($values),
+            new FetchingIterator($this->createCallbackWithValues($values))
+        );
+    }
+}
