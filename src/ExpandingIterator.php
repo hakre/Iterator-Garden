d920fed9ddf9fd4cc770883935b5a50104a5eb13
hakre <hanskrentel@yahoo.de>
2013-07-19 02:01:20 +0200

    Added ExpandingIterator

---
+++ src/ExpandingIterator.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Iterator Garden
+ */
+
+class ExpandingIterator extends TraversableDecorator
+{
+    const CATCH_GET_CHILD = 16;
+
+    /**
+     * @var Iterator
+     */
+    private $subject;
+    private $flags;
+
+    public function __construct(Iterator $iterator, $flags = 0)
+    {
+        $this->subject = $iterator;
+        $this->flags = (int) $flags;
+
+        parent::__construct(new EmptyIterator());
+    }
+
+    public function rewind()
+    {
+        $this->subject->rewind();
+        $this->push();
+    }
+
+    public function next()
+    {
+        $this->traversable->next();
+
+        if ($this->traversable->valid()) {
+            return;
+        }
+
+        $this->subject->next();
+        $this->push();
+    }
+
+    private function push()
+    {
+        ($this->flags & self::CATCH_GET_CHILD)
+            ? $this->pushCatch()
+            : $this->pushStd();
+    }
+
+    private function pushCatch() {
+        while ($this->subject->valid()) {
+            try {
+                $this->traversable = new IteratorIterator($this->subject->current());
+            } catch (Exception $e) {
+                $this->subject->next();
+                continue;
+            }
+            $this->traversable->rewind();
+            break;
+        }
+    }
+
+    private function pushStd() {
+        if ($this->subject->valid()) {
+            $this->traversable = new IteratorIterator($this->subject->current());
+            $this->traversable->rewind();
+        }
+    }
+}
---
+++ test/ExpandingIteratorTest.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Iterator Garden
+ */
+
+class ExpandingIteratorTest extends IteratorTestCase
+{
+    public function testConstructor()
+    {
+        $expanding = new ExpandingIterator($this->getIteratorOfIterators());
+
+        $this->assertInstanceOf('ExpandingIterator', $expanding);
+    }
+
+    public function testException()
+    {
+        $withExceptions = $this->getIteratorOfIteratorsAndStrings();
+
+        $expanding = new ExpandingIterator($withExceptions);
+        $thrown    = FALSE;
+        try {
+            $this->addToAssertionCount(1);
+            $count = iterator_count($expanding);
+        } catch (Exception $e) {
+            $thrown = TRUE;
+        }
+        $this->assertTrue($thrown, 'Exception was thrown');
+
+        $expanding = new ExpandingIterator($withExceptions, ExpandingIterator::CATCH_GET_CHILD);
+        $count     = iterator_count($expanding);
+        $this->assertSame(9, $count);
+    }
+
+    public function testIterationValues()
+    {
+        $values = [1, 2, 3, 'a', 'b', 'c', 4, 5, 6, 'd', 'e', 'f'];
+
+        $expanding = new ExpandingIterator($this->getIteratorOfIterators());
+
+        $this->assertIterationValues($values, $expanding);
+    }
+
+    private function getIteratorOfIteratorsAndStrings()
+    {
+        $array = [
+            new ArrayIterator(range(1, 3)),
+            'exception',
+            new ArrayIterator(range(4, 6)),
+            'exception',
+            'exception',
+            new ArrayIterator(range(7, 9)),
+        ];
+        return new ArrayIterator($array);
+    }
+
+    private function getIteratorOfIterators()
+    {
+        $array = [
+            [1, 3],
+            ['a', 'c'],
+            [4, 6],
+            ['d', 'f'],
+        ];
+        $it    = new ArrayIterator($array);
+        return new DecoratingIterator($it, function (array $array) {
+            list($from, $to) = $array;
+            return new ArrayIterator(range($from, $to));
+        });
+    }
+}
