80dbc36745e535b7a2846cd1c1bdd940908ed6fc
hakre <hanskrentel@yahoo.de>
2013-08-07 02:04:31 +0200

    Added ForeachIterator Class B
    Class Overview:
    
    -  A: Wrapped with SPL existing
    -  B: reset() / current() / key() / next() (/end() / prev())
    -  C: generator (PHP 5.5)

---
+++ src/ForeachIteratorB.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Iterator Garden
+ */
+
+/**
+ * ForeachIterator Class B
+ *
+ *  A: Wrapped with SPL existing
+ *  B: reset() / current() / key() / next() (/end() / prev())
+ *  C: generator (PHP 5.5)
+ */
+class ForeachIteratorB implements Iterator
+{
+    private $subject;
+    private $key;
+
+    /**
+     * @param array|object $foreachAble
+     */
+    public function __construct($foreachAble)
+    {
+        $this->subject = $foreachAble;
+    }
+
+    public function rewind() {
+        reset($this->subject);
+        $this->key = key($this->subject);
+    }
+
+    public function valid() {
+        return NULL !== $this->key;
+    }
+
+    public function current() {
+        return current($this->subject);
+    }
+
+    public function key() {
+        return $this->key;
+    }
+
+    public function next() {
+        next($this->subject);
+        $this->key = key($this->subject);
+    }
+}
