6879c2950f0660cad93f04eaf20ecd4ed28ddf45
hakre <hanskrentel@yahoo.de>
2013-08-07 02:40:27 +0200

    Added InstanceOfFilter

---
+++ src/Filter/InstanceOfFilter.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Iterator Garden
+ */
+
+/**
+ * Class InstanceOfFilter
+ *
+ * General purpose type filter.
+ */
+class InstanceOfFilter extends FilterIterator
+{
+    private $className;
+
+    public function __construct(Iterator $iterator, $className) {
+        parent::__construct($iterator);
+
+        $this->className = $className;
+    }
+
+    public function accept() {
+        return ($this->getInnerIterator()->current() instanceof $this->className);
+    }
+}
