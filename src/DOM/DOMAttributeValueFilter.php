30421d104fa5d56ef1d3a95b8fc4aaced95f8cf3
hakre <hanskrentel@yahoo.de>
2013-08-07 03:02:55 +0200

    Added DOMAttributeValueFilter

---
+++ src/DOM/DOMAttributeValueFilter.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Iterator-Garden
+ */
+
+/**
+ * Class DOMAttributeValueFilter
+ */
+class DOMAttributeValueFilter extends FilterIterator
+{
+    private $attribute, $value;
+
+    public function __construct(Iterator $iterator, $attribute, $value)
+    {
+        parent::__construct(new DOMAttributeFilter($iterator, $attribute));
+
+        $this->attribute = $attribute;
+        $this->value     = $value;
+    }
+
+    public function accept()
+    {
+        return $this->current()->getAttribute($this->attribute) === $this->value;
+    }
+}
