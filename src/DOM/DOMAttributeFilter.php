dd6bfdf9f299b5bd2df07ff6a1c223e6a9a0db03
hakre <hanskrentel@yahoo.de>
2013-08-07 03:01:39 +0200

    Added DOMAttributeFilter

---
+++ src/DOM/DOMAttributeFilter.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Iterator-Garden
+ */
+
+/**
+ * Class DOMAttributeFilter
+ */
+class DOMAttributeFilter extends FilterIterator
+{
+    private $attributeName;
+
+    public function __construct(Iterator $iterator, $attributeName)
+    {
+        parent::__construct(new InstanceOfFilter($iterator, 'DOMElement'));
+
+        $this->attributeName = $attributeName;
+    }
+
+    public function accept()
+    {
+        return $this->current()->hasAttribute($this->attributeName);
+    }
+}
