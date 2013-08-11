a214fc445b2156945037b2d30cecd1165ee88205
hakre <hanskrentel@yahoo.de>
2013-08-11 13:17:49 +0200

    Added DecoratingIteratorDecorator
    Very similar to DecoratingIterator sometimes in PHP < 5.5 a
    IteratorDecorator is required instead of a IteratorIterator as
    IteratorIterator gives warnings on "Illegal type returned from
    Iterator::key()" when caching the key value per each iteration.

---
+++ src/DecoratingIteratorDecorator.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Iterator Garden
+ */
+
+/**
+ * Class DecoratingIteratorDecorator
+ */
+class DecoratingIteratorDecorator extends IteratorDecorator
+{
+    private $decorator;
+
+    function __construct(Iterator $iterator, $decorator)
+    {
+        $this->decorator = $decorator;
+        parent::__construct($iterator);
+    }
+
+    function current()
+    {
+        $decorator = $this->decorator;
+        if (is_callable($decorator)) {
+            return call_user_func($decorator, $this->traversable->current());
+        } elseif (class_exists($decorator)) {
+            return new $decorator($this->traversable->current());
+        } else {
+            throw new Exception('Not a valid decorator: ' . var_export($decorator, TRUE));
+        }
+    }
+}
