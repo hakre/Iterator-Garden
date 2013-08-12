552372666dbc6efca07ceccaabed738643d9fdeb
hakre <hanskrentel@yahoo.de>
2013-08-12 19:23:06 +0200

    Added DebugCountingIteratorDecorator

---
+++ src/Debug/DebugCountingIteratorDecorator.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * IteratorGarden
+ */
+
+class DebugCountingIteratorDecorator extends IteratorDecorator
+{
+    private $rewindCount;
+
+    public function getRewindCount() {
+        return $this->rewindCount;
+    }
+
+    public function rewind()
+    {
+        $this->rewindCount++;
+        parent::rewind();
+    }
+}
---
+++ test/Debug/DebugCountingIteratorDecoratorTest.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ *Iterator Garden
+ */
+
+class DebugCountingIteratorDecoratorTest extends IteratorTestCase {
+
+    /**
+     * @test
+     */
+    public function canInstantiate() {
+        $it = new DebugCountingIteratorDecorator(new EmptyIterator());
+    }
+
+    /**
+     * @test
+     */
+    public function countRewinds() {
+        $it = new DebugCountingIteratorDecorator(new EmptyIterator());
+        $this->assertNull($it->getRewindCount());
+        $it->rewind();
+        $this->assertEquals(1, $it->getRewindCount());
+    }
+}
