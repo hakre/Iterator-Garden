d4c577b073ecbc2a893e249d2f0117ccad8d02da
hakre <hanskrentel@yahoo.de>
2013-10-06 12:24:01 +0200

    Added DecoratingIteratorWithArguments
    Subtype of DecoratingIterator with optional arguments array.
    
    NOTE: So far there is no use-case, tests are missing.

---
+++ src/DecoratingIteratorWithArguments.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Iterator Garden
+ */
+
+/**
+ * Class DecoratingIteratorWithArguments
+ *
+ * @author Marcus Börger (2008); see DecoratingIterator
+ * @author hakre (2013)
+ */
+class xxxDecoratingIteratorWithArguments extends IteratorIterator
+{
+    private $decorator;
+    private $args;
+
+    function __construct(Traversable $iterator, $decorator, array $args = array())
+    {
+        $this->decorator = $decorator;
+        $this->args      = $args ? array_combine(range(1, count($args)), $args) : $args;
+        parent::__construct($iterator);
+    }
+
+    function current()
+    {
+        $decorator = $this->decorator;
+        if (is_callable($decorator)) {
+            $args = array(parent::current()) + $this->args;
+            return call_user_func_array($decorator, $args);
+        } elseif (class_exists($decorator)) {
+            if (!$this->args) {
+                return new $decorator(parent::current());
+            } else {
+                $reflected = new ReflectionClass($decorator);
+                return $reflected->newInstanceArgs(array(parent::current()) + $this->args);
+            }
+        } else {
+            throw new Exception('Not a valid decorator: ' . var_export($decorator, TRUE));
+        }
+    }
+}
