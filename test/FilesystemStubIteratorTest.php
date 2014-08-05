99ae37587678c849dc1dd6703cc585c17b46bf54
hakre <hanskrentel@yahoo.de>
2014-08-05 22:34:59 +0200

    Added composer support
    Also expaneded file-comments / added plates and added COPYING file.

--- test/FilesystemStubIteratorTest.php
+++ test/FilesystemStubIteratorTest.php
@@ -1,6 +1,20 @@
 <?php
-/**
- * Iterator Garden
+/*
+ * Iterator Garden - Let Iterators grow like flowers in the garden.
+ * Copyright 2013, 1014 hakre <http://hakre.wordpress.com/>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 class FilesystemStubIteratorTest extends IteratorTestCase
